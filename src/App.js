469f95627c21bc15bce179f5185729ec188eb596
aimee-liang <aimeeeliang@gmail.com>
2020-12-24 15:42:32 -0500

    Moved Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import {Route, Switch} from "react-router-dom"
+import Navbar from "./Components/Navbar"
 import Home from "./Components/Home"
 import Nominees from "./Components/Nominees"
 
 const App = props => {
   return(
     <>
+    
+    <Navbar />
+    
     <Switch>
       <Route path ="/home" render={() => <Home/> } />
       <Route path ="/nominees" render={()=> <Nominees/>} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,8 @@
 import React from "react"
-import Navbar from "./Navbar"
 
 const Home = props => {
     return(
-        <>
-            <Navbar />
+        <>    
             
         </>
     )
