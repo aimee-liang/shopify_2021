ac90694e5a15865f59b5c92bdd563dd5c688626a
aimee-liang <aimeeeliang@gmail.com>
2020-12-24 15:12:29 -0500

    Imported Switch and Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
+import {Route, Switch} from "react-router-dom"
+import Home from "./Components/Home"
 
 const App = props => {
   return(
     <>
-    <
+    <Switch>
+      <Route path ="/home" render={() => <Home/> } />
+    </Switch>
     </>
   )
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = props => {
+    return(
+        <>
+        <h4>The Shoppies</h4>
+        </>
+    )
+}
+
+export default Header
